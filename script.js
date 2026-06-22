1936a40c48e9e557163d8a98b7cbf76ac4d60721
Vlad <vladlealoliv@gmail.com>
2026-06-22 09:59:51 -0300

    transformando em funcao denovo

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const focoBt = document.querySelector(".app__card-button--foco")
 const curtoBt = document.querySelector(".app__card-button--curto")
 const longoBt = document.querySelector(".app__card-button--longo")
 const startBt = document.querySelector(".app__card-primary-button")
+const botoes = document.querySelectorAll(".app__card-button") 
+//O querrySelectorAll pega mais de um elemnto, fazendo um array. Ou seja, a const botoes é uma array
 
 const bannerImg = document.querySelector(".app__image")
 const bannerTitle = document.querySelector(".app__title")
@@ -11,43 +13,27 @@ const focoTime = 1500
 const curtoTime = 300
 const longoTime = 900
 
-function switchContexto(contexto){
+function switchContexto(contexto){ //A função esta removendo o "active de todos os botões", para depois ser colocada no botão usado.
+    botoes.forEach(contexto => {  //Devo usar forEach porque se trata de um array. 
+         contexto.classList.remove("active")
+    });
+
     html.setAttribute('data-contexto', contexto)
     bannerImg.setAttribute("src", `/imagens/${contexto}.png`)
-    switch (contexto){
-        case "foco":
-            curtoBt.classList.remove("active")
-            focoBt.classList.add("active")
-            longoBt.classList.remove("active")
-            bannerTitle.innerHTML = `Otimize sua produtividade,<br />
-             <strong class="app__title-strong">mergulhe no que importa.</strong>`
-             break
-        case "descanso-curto":
-            curtoBt.classList.add("active")
-            focoBt.classList.remove("active")
-            longoBt.classList.remove("active")
-            bannerTitle.innerHTML = `Que tal dar uma respirada?<br />
-            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-            break
-        case "descanso-longo":
-            curtoBt.classList.remove("active")
-            focoBt.classList.remove("active")   
-            longoBt.classList.add("active")
-            bannerTitle.innerHTML = `Hora de voltar à superfície.<br />
-            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
-
-    }
 }
 
 
 focoBt.addEventListener('click', () => {
     switchContexto("foco")
+    focoBt.classList.add("active")
 })
 
 curtoBt.addEventListener('click', () => {
     switchContexto("descanso-curto")
+    curtoBt.classList.add("active")
 })
 
 longoBt.addEventListener('click', () => {
     switchContexto("descanso-longo")
+    longoBt.classList.add("active")
 })
