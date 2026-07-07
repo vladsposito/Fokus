72bf386289dfa4a91d532eb07c6c5d2aa14e37c8
Vlad <vladlealoliv@gmail.com>
2026-07-06 23:29:31 -0300

    Consumindo API JSON

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,19 @@
 const addTaskBt = document.querySelector(".app__button--add-task")
 
 const taskCard = document.querySelector(".app__form-add-task")
+const textArea = document.querySelector(".app__form-textarea")
+
+const taskList = []
 
 addTaskBt.addEventListener("click", ()=>{
     taskCard.classList.toggle("hidden")
+})
+
+taskCard.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const tasks = {
+        taskName: textArea.value
+    }
+    taskList.push(tasks)
+    localStorage.setItem("taskList", JSON.stringify(taskList))
 })
