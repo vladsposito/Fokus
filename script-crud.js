4031a3db508aa6901ce4864fb5b35ccb001991df
Vlad <vladlealoliv@gmail.com>
2026-07-10 11:02:41 -0300

    criando funcao createTask

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,36 @@ const textArea = document.querySelector(".app__form-textarea")
 
 const taskList = []
 
+function createTask(){
+    const li = document.createElement("li")
+    li.classList.add("app__section-task-list-item")
+
+    const svg = document.createElement("svg")
+    svg.innerHTML = `
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+    </svg>
+    `
+
+    const p = document.createElement("p")
+    p.classList.add("app__section-task-list-item-description")
+    p.textContent(tasks.taskName)
+
+    const button = document.createElement("button")
+    button.classList.add("app_button-edit")
+
+    const img = document.createElement("img")
+    img.setAttribute("src", "/imagens/edit.png")
+
+    button.append(img)
+    li.append(svg)
+    li.append(p)
+    li.append(button)
+
+
+}
+
 addTaskBt.addEventListener("click", ()=>{
     taskCard.classList.toggle("hidden")
 })
